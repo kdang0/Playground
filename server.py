3ef7cea32b8c52382cb4f954bf63ba4b26032fba
kdang0 <73298064+kdang0@users.noreply.github.com>
2022-09-29 18:00:56 -0400

    Playground Assignment

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/play/<x>')
 @app.route('/play')
-def renderPlay(x=3):
-    return render_template('playground.html', times = int(x))
+@app.route('/play/<int:x>')
+@app.route('/play/<int:x>/<string:color>')
+def renderPlay(x=3, color="rgb(73, 145, 226)"):
+    return render_template('playground.html', times = x, color= color)
 
 
 if __name__ == "__main__":
